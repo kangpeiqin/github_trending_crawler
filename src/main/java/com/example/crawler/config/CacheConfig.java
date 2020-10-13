e487844a8151393ddcc77851cdb2e73c5f16f59e
kangpeiqin <1060647301@qq.com>
2020-10-14 00:28:46 +0800

    更新README.md

---
+++ src/main/java/com/example/crawler/config/CacheConfig.java
@@ -0,0 +1,13 @@
+package com.example.crawler.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author kpq
+ * @since 1.0.0
+ */
+@Configuration
+public class CacheConfig {
+
+}
